f42f9d3608bb550cff168f4ae513465936a50a5f
harunar21 <harun@rekosistem.com>
2023-09-20 21:33:27 +0700

    - Feat: add home component

---
+++ src/app/constants/index.ts
@@ -0,0 +1,7 @@
+import { Constants } from "@constants";
+
+export const HOME_TITLE: string = "Discover & Share";
+export const HOME_SUBTITLE: string = "AI-Powered Prompts";
+export const HOME_TITLE_DESC: string =
+  Constants.APP_NAME +
+  " is an open-source AI prompting tool for modern world to discover, create and share creative prompts";
